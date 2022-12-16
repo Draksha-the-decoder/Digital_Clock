898270680130613190d5b74ab03ec4f213a51058
Draksha <96646429+Draksha-the-decoder@users.noreply.github.com>
2022-12-16 13:44:09 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,7 +1,16 @@
-setInterval(()  =>{
+setInterval(()  =>
+{
     a=new Date();
-    let time = a.getHours() + " : " + a.getMinutes() +" : " + a.getSeconds();
-    let date = a.getDate()+" . "+ a.getMonth()+" . "+ a.getFullYear();
+    let time;
+    if(a.getHours()>12)
+    {
+        time = (a.getHours()-12) + " : " + a.getMinutes() +" : " + a.getSeconds() + " PM";
+    }
+    else
+    {
+        time = hours + " : " + a.getMinutes() +" : " + a.getSeconds() + " AM";
+    }    
+    let date = a.getDate()+" . "+ (a.getMonth()+1)+" . "+ a.getFullYear();
     document.getElementById("date").innerHTML = date;
     document.getElementById("time").innerHTML = time ;
-    },1000);
+},1000);
